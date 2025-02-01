872d6b130d916b1cb7361adaa4ce88173de24ab3
CongKony <chicong442004@gmail.com>
2025-02-01 14:28:42 +0700

    feat: Add routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,6 @@
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home/home';
+
+const publicRoutes = [{ path: '/', component: Home }];
+
+export { publicRoutes };
